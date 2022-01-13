56e1f1c8eb36fbb1f93caa12b9fdf5cde4a3a043
AlbertVich <albert.vichv@gmail.com>
2022-01-13 13:22:25 +0100

    GrabDone

--- Assets/Scripts/PickUpObject.cs
+++ Assets/Scripts/PickUpObject.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PickUpObject : MonoBehaviour
 {
@@ -9,32 +10,60 @@ public class PickUpObject : MonoBehaviour
     public Transform interactionZone;
 
 
-    void Update()
+    [SerializeField]
+    private PlayerInput playerInput;
+
+    private InputAction grabAction;
+
+
+
+    private void Awake()
+    {
+        grabAction = playerInput.actions["Grab"];
+
+    }
+
+
+    private void OnEnable()
+    {
+        grabAction.performed += _ => StartGrab();
+
+
+    }
+
+    private void OnDisable()
+    {
+        grabAction.performed += _ => StartGrab();
+
+    }
+
+
+
+
+    void StartGrab()
     {
         if(ObjectToPickUp != null && ObjectToPickUp.GetComponent<PickableObject>().isPickable == true && PickedObject == null)
         {
-            if (Input.GetKeyDown(KeyCode.Q))
-            {
+
                 PickedObject = ObjectToPickUp;
                 PickedObject.GetComponent<PickableObject>().isPickable = false;
                 PickedObject.transform.SetParent(interactionZone);
                 PickedObject.transform.position = interactionZone.position;
                 PickedObject.GetComponent<Rigidbody>().useGravity = false;
                 PickedObject.GetComponent<Rigidbody>().isKinematic = true;
-            }
+          
         }
 
         else if (PickedObject != null)
         {
-            if (Input.GetKeyDown(KeyCode.Q))
-            {
+
                 PickedObject.GetComponent<PickableObject>().isPickable = true;
                 PickedObject.transform.SetParent(null);
                 PickedObject.GetComponent<Rigidbody>().useGravity = true;
                 PickedObject.GetComponent<Rigidbody>().isKinematic = false;
                 PickedObject = null;
 
-            }
+            
         }
     }
 }
