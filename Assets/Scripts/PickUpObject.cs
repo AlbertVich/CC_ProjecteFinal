3289c7fa2d6041621da3f006bd4432c8f83bef2a
AlbertVich <albert.vichv@gmail.com>
2022-01-13 13:05:29 +0100

    PickableWork

---
+++ Assets/Scripts/PickUpObject.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpObject : MonoBehaviour
+{
+    public GameObject ObjectToPickUp;
+    public GameObject PickedObject;
+    public Transform interactionZone;
+
+
+    void Update()
+    {
+        if(ObjectToPickUp != null && ObjectToPickUp.GetComponent<PickableObject>().isPickable == true && PickedObject == null)
+        {
+            if (Input.GetKeyDown(KeyCode.Q))
+            {
+                PickedObject = ObjectToPickUp;
+                PickedObject.GetComponent<PickableObject>().isPickable = false;
+                PickedObject.transform.SetParent(interactionZone);
+                PickedObject.transform.position = interactionZone.position;
+                PickedObject.GetComponent<Rigidbody>().useGravity = false;
+                PickedObject.GetComponent<Rigidbody>().isKinematic = true;
+            }
+        }
+
+        else if (PickedObject != null)
+        {
+            if (Input.GetKeyDown(KeyCode.Q))
+            {
+                PickedObject.GetComponent<PickableObject>().isPickable = true;
+                PickedObject.transform.SetParent(null);
+                PickedObject.GetComponent<Rigidbody>().useGravity = true;
+                PickedObject.GetComponent<Rigidbody>().isKinematic = false;
+                PickedObject = null;
+
+            }
+        }
+    }
+}
